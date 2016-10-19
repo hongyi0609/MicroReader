143a4e763d14296c2f385cdfd048a653e4f962c4
蔡小木 <whucaiyao@gmail.com>
2016-10-19 09:41:34 +0800

    update library

--- app/src/main/assets/info.js
+++ app/src/main/assets/info.js
@@ -1,4 +1,4 @@
-          //判断图片是否加载完成
+          //?卸????????????
           function IsImageLoaded()
           {
               var img=event.srcElement;
@@ -14,14 +14,14 @@
               {
                    if(img.complete)
                    {
-						//ios 浏览图片使用
+						//ios ????????
                        var imagesrc = img.attributes['originsrc'].nodeValue;
                        //document.location = imagesrc.replace("http://","ruanmeipic://");
                        ProxyClickPicture.clickImg(imagesrc);
                    }
                    else
                    {
-                       //未加载完成时，暂不放大图片，以后可以修改
+                       //未?????????????????????????????
                        //img.src = img.attributes['originsrc'].nodeValue;
                    }
               }
